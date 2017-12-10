722f069892db471e3d2442e9d47f9c28aaf03dd8
Alex Choi <wanchoi211@users.noreply.github.com>
2017-12-09 20:29:05 -0800

    twoSum added

---
+++ src/twoSum.js
@@ -0,0 +1,13 @@
+// Original Source: https://leetcode.com/problems/two-sum/description/
+
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numsArray, target) {
+	return;
+}
