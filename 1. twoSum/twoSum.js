98f0c014b949ce905c6d7a293334a898a98b8f1a
Alex Choi <wanchoi211@users.noreply.github.com>
2017-12-09 21:23:43 -0800

    Update twoSum.js

--- 1. twoSum/twoSum.js
+++ 1. twoSum/twoSum.js
@@ -9,19 +9,5 @@
  * @return {number[]}
  */
 var twoSum = function(numsArray, target) {
-  var hashTable = {};
-  var currNum;
-  var complement;
-
-  for (var i = 0; i < numsArray.length; i++) {
-    currNum = numsArray[i];
-    complement = target - currNum;
-
-    if (hashTable[complement] !== undefined) {
-      return [hashTable[complement], i];
-    }
-
-    hashTable[currNum] = i;
-  }
-  return [];
+  return;
 }
